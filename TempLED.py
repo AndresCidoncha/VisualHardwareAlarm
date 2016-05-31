b8098139b37f02cc702c32ef724fc11baec1fa44
Andrés Cidoncha Carballo <alu0100766806@ull.edu.es>
2016-05-31 23:21:02 +0100

    Fixing

--- TempLED.py
+++ TempLED.py
@@ -2,14 +2,28 @@ import SystemInfo
 import serial
 import serial.tools.list_ports
 
-# You can
+# You must change this for your Arduino VID:PID!!
 ARDUINO_ID = "1A86:7523"
 
+# Get the GPU's temperature sensor
+system = SystemInfo.SystemInfo()
+graphic_temp_sensor = None
+
+for sensor in system.GPU.Sensors:
+    if sensor.SensorType == "Temperature":
+        graphic_temp_sensor = sensor
+
+# Get the Arduino port
 arduino_list = serial.tools.list_ports.grep(ARDUINO_ID)
 
 for device in arduino_list:
     arduino_port = device.device
 
 ser = serial.Serial(arduino_port)
-print(ser)
-ser.close()
+try:
+    while True:
+        temp = graphic_temp_sensor.getValue()
+        print(temp)
+except Exception:
+    print("Exit!")
+    ser.close()
