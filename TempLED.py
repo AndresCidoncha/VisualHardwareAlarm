c3856ef233be488ec14f7bbb4ccf6388037d1ba6
Andrés Cidoncha Carballo <alu0100766806@ull.edu.es>
2016-06-07 00:24:16 +0100

    Constants, Baudrate, New Functions

--- TempLED.py
+++ TempLED.py
@@ -1,17 +1,40 @@
+# -*- coding: utf-8 -*-
+
 import SystemInfo
 import serial
 import serial.tools.list_ports
+from time import sleep
 
 # You must change this for your Arduino VID:PID!!
 ARDUINO_ID = "1A86:7523"
 
+
+NORMAL = "0"
+THEATER_CHASE = "1"
+RAINBOW = "2"
+RAINBOW_CYCLE = "3"
+BREATHING = "4"
+
+def select_mode(load):
+    if(load > 30):
+        return THEATER_CHASE
+    else:
+        return BREATHING
+
+
+def select_color(temp):
+    return "0 255 255"
+
 # Get the GPU's temperature sensor
 system = SystemInfo.SystemInfo()
 graphic_temp_sensor = None
+graphic_load_sensor = None
 
 for sensor in system.GPU.Sensors:
     if sensor.SensorType == "Temperature":
         graphic_temp_sensor = sensor
+    if sensor.SensorType == "Load" and sensor.Name == "GPU Core":
+        graphic_load_sensor = sensor
 
 # Get the Arduino port
 arduino_list = serial.tools.list_ports.grep(ARDUINO_ID)
@@ -19,11 +42,25 @@ arduino_list = serial.tools.list_ports.grep(ARDUINO_ID)
 for device in arduino_list:
     arduino_port = device.device
 
-ser = serial.Serial(arduino_port)
+ser = serial.Serial(arduino_port, 115200)
+
 try:
+    n = 0
+    m_load = 0
+    m_temp = 0
     while True:
+        load = graphic_load_sensor.getValue()
+        m_load += load
         temp = graphic_temp_sensor.getValue()
-        print(temp)
-except Exception:
+        m_temp += temp
+        n += 1
+        print("Load:", load, "%", "Temp:", temp, "Cº")
+        command = select_mode(load) + ' ' + select_color(temp)
+        ser.write(command.encode('UTF-8'))
+        sleep(3)
+
+except KeyboardInterrupt:
+    print("Medium Load:", m_load/n, "%", "Medium Temp:", m_temp/n, "Cº")
+    ser.write("0".encode('UTF-8'))
     print("Exit!")
     ser.close()
